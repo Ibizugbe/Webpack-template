58668322d93ff0fff9444c109dc966f283f1a08f
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-06 11:09:19 +0100

    add webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
   ],
+  optimization: {
+    runtimeChunk: "single",
+  },
 };
